8d6140c975bfef9c79945899dd2d385b98cfe328
Scrip0 <lavrenenko.2003@gmail.com>
2022-08-10 11:58:51 -0400

    Some minor changes

--- app/src/main/java/com/scrip0/sporty/ui/fragments/RegistrationFragment.kt
+++ app/src/main/java/com/scrip0/sporty/ui/fragments/RegistrationFragment.kt
@@ -3,5 +3,5 @@ package com.scrip0.sporty.ui.fragments
 import androidx.fragment.app.Fragment
 import com.scrip0.sporty.R
 
-class LoginFragment : Fragment(R.layout.login_fragment) {
+class RegistrationFragment : Fragment(R.layout.registration_fragment) {
 }
